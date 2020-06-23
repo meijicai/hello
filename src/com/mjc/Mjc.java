9d32c8d56ae47cb7c03e53159977e4e2b44b9e38
meijicai <956294823@qq.com>
2020-06-23 21:56:49 +0800

    Initial commit

---
+++ src/com/mjc/Mjc.java
@@ -0,0 +1,16 @@
+package com.mjc;
+
+/**
+ * @author mjc
+ * @data 2020/6/23 0023-16:59
+ */
+public class mjc {
+  private  String a;
+  private   String b;
+
+public static void main(String args []){
+
+}
+
+
+        }
